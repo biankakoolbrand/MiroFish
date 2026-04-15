15562dbf2e3f26a41bbff7b3c8e1d8bbad5c3f12
biankakoolbrand <bianka.koolbrand@gmail.com>
2026-04-15 19:59:49 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,17 +11,17 @@ export default defineConfig({
       '@locales': path.resolve(__dirname, '../locales')
     }
   },
-  server: {
-    host: true,
-    allowedHosts: ['qgo3ojwu3rw36shgngk80ykd.92.176.76.208.sslip.io'],
-    port: 3000,
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5001',
-        changeOrigin: true,
-        secure: false
-      }
+server: {
+  host: true,
+  allowedHosts: ['qgo3ojwu3rw36shgngk80ykd.92.176.76.208.sslip.io'],
+  port: 3000,
+  open: true,
+  proxy: {
+    '/api': {
+      target: 'http://10.0.1.14:5001',
+      changeOrigin: true,
+      secure: false
     }
   }
+}
 })
